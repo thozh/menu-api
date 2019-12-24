fea071efc52541c022d29eb3df7df77a41eecf5a
Thomas <thz@MacBook-Pro-de-Thomas.local>
2019-12-24 11:54:40 +0100

    create API endpoints and add payload validation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { Logger, ValidationPipe } from '@nestjs/common';
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: true }));
+
   await app.listen(process.env.PORT);
+  Logger.log(process.env.PORT);
 }
 bootstrap();
