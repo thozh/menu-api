cccd532730b0959d2be8d1967fedafcbf858cda0
Thomas <thomaszhou974@gmail.com>
2019-12-24 12:33:11 +0100

    Secure API with Auth0 authentication

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { Logger, ValidationPipe } from '@nestjs/common';
+
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ disableErrorMessages: true }));
 
   await app.listen(process.env.PORT);
